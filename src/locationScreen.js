f3af28bb011ede639900f3d6bb0fe058bff589ce
Unknown <joppe_meijers@hotmail.com>
2019-09-25 10:47:20 +0200

    first commit

--- src/locationScreen.js
+++ src/locationScreen.js
@@ -1,24 +1,79 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Image, Button, ScrollView} from 'react-native';
 
 
+class locationScreen extends Component {
+    render() {
+      return (
+        <ScrollView>
+          
+        <View style={styles.container}>
+          <View>
+            <Text style={styles.welcome}>Welcome to Location!</Text>
+          </View>
+          <View>
+            <Text style={styles.welcome}>Welcome to Location!</Text>
+          </View>
+          <View>
+            <Text style={styles.welcome}>Welcome to Location!</Text>
+          </View>
+          <View>
+            <Text style={styles.welcome}>Welcome to Location!</Text>
+          </View>
+          <View>
+            <Text style={styles.welcome}>Welcome to Location!</Text>
+          </View>
+          <View>
+            <Text style={styles.welcome}>Welcome to Location!</Text>
+          </View>
+          <View>
+            <Text style={styles.welcome}>Welcome to Location!</Text>
+          </View>
+          <View>
+            <Text style={styles.welcome}>Welcome to Location!</Text>
+          </View>
+          <View>
+            <Text style={styles.welcome}>Welcome to Location!</Text>
+          </View>
+          <View>
+            <Text style={styles.welcome}>Welcome to Location!</Text>
+          </View>
+          <View>
+            <Text style={styles.welcome}>Welcome to Location!</Text>
+          </View>
+          <View>
+            <Text style={styles.welcome}>Welcome to Location!</Text>
+          </View>
+          <View>
+            <Text style={styles.welcome}>Welcome to Location!</Text>
+          </View>
+        </View>
+      </ScrollView>
+      );
+    }
+  }
 
-class locationScreen extends Component {
-
-
-    render() {
-      return (
-      
-            <View>
-                <Text> Location screen</Text>
-            </View>
-
-      );
-    }
-  }
-
-  const styles = StyleSheet.create({
-   
-  });
-  
-  export default locationScreen;
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+      marginTop: 50,
+    },
+    instructions: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 15,
+    },
+    button: {
+  
+    }
+  });
+  
+  export default locationScreen;
---
+++ src/spots/reviews.js
@@ -0,0 +1,40 @@
+
+export {reviews};
+    
+
+
+var reviews = [
+        {
+            name: 'Barberistas',
+            location: 'Geleen',
+            username: 'Joppe Meijers',
+            Rating: 4,
+            date: '01-01-19',
+            subject: 'Heerlijke filter koffie',
+            desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius hendrerit dignissim. Sed eu quam imperdiet, dapibus lectus nec, maximus arcu.'
+        },
+        {
+            name: 'Barberistas',
+            location: 'Geleen',
+            username: 'Raoul Frissen',
+            Rating: 5,
+            date: '01-01-19',
+            subject: 'De beste cappucino',
+            desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius hendrerit dignissim. Sed eu quam imperdiet, dapibus lectus nec, maximus arcu.'
+        },
+
+        {
+            name: 'Barberistas',
+            location: 'Geleen',
+            username: 'Inge Tillmans',
+            Rating: 3,
+            date: '01-01-19',
+            subject: 'Mooie late art!',
+            desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius hendrerit dignissim. Sed eu quam imperdiet, dapibus lectus nec, maximus arcu.'
+        },
+
+        
+      ]
+
+    
+    

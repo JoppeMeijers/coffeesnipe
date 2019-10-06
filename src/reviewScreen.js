7986881e0829123a1e5c16725860a1d3d733fdd7
Unknown <joppe_meijers@hotmail.com>
2019-10-06 21:08:14 +0200

    profile-review screen done

--- src/reviewScreen.js
+++ src/reviewScreen.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, Button, ScrollView} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image,ScrollView} from 'react-native';
 import SafeAreaView from 'react-native-safe-area-view';
-import { Avatar, Divider, List, ListItem, Rating } from 'react-native-elements';
+import { Avatar, Divider,Button, List, ListItem, Rating } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 
@@ -30,19 +31,21 @@ class reviewScreen extends Component {
         <ScrollView>
           <SafeAreaView>
         <View style={styles.container}>
-            <Text>{this.subject}</Text>
-            <Text>{this.name}</Text>
-            <Text>{this.location}</Text>
-            <Text>{this.date}</Text>
-            <Rating 
-              type='star'
-              ratingCount={5}
-              imageSize={10}
-              startingValue={this.rating}
-            readonly
-            style={styles.rating}
-            />
+          <Text style={styles.locationName}>{this.name}</Text>  
+          <Text style={styles.date}>{this.date}</Text>
+           <Rating type='star' ratingCount={5} imageSize={10} startingValue={this.rating} readonly style={styles.rating} />
+
+             <Divider style={styles.divider} />
+              <Text style={styles.subjectName}>{this.subject}</Text>
             <Text>{this.desc}</Text>
+
+            <Divider style={styles.divider} />
+            <View style={styles.locationDetails}>
+            <Text style={styles.location}> <Icon name="map-marker" size={18} color="#d85454" /> {this.location}</Text>
+            <Text style={styles.right}>Routebeschrijving  <Icon name="arrow-right" size={13} color="#0000FF" /> </Text>
+            </View>
+            <Button style={styles.changeButton}  title="Bewerk review" />
+            <Button style={styles.changeButton}  title="Voeg foto toe" />
         </View>
         </SafeAreaView>
       </ScrollView>
@@ -53,53 +56,52 @@ class reviewScreen extends Component {
   const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        padding: 30,
         paddingTop: 20,
-    },
-    welcome:{
-      marginTop: 15,
-        color: 'black',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    userName:{
-      fontWeight: '800',
-    },
-      information:{
-        marginTop: 30,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        flex: 1,
-
-    },
-    block:{
-      marginLeft: 35,
-      marginRight: 35,
-      alignItems: 'center',
-      textAlign: 'center',
-    },
-    counting:{
-      marginTop: 10,
-      fontWeight: '800',
-      fontSize: 21,
-    },
-    title:{
-      color: '#000',  
-      textAlign: 'left',
-      fontSize: 15,
-      fontWeight: '600',
-      marginTop: 30,
-      marginLeft: 20,
-    },
-    reviews :{
-      marginLeft: 10,
     },
     rating:{
         marginTop: 5,
         marginBottom: 5,
         alignItems: 'flex-start',
       },
+    divider:{
+      backgroundColor: 'grey',
+      marginTop: 15,
+      marginBottom: 15,
+    },
+    locationName:{
+      fontWeight: '800',
+      fontSize: 21
+    },
+    date:{
+      fontStyle: 'italic',
+      marginTop: 5,
+      fontWeight: '300',
+    },
+    subjectName:{
+      fontWeight: '600',
+      fontSize: 16,
+      marginBottom: 5,
+    },
+    changeButton:{
+      marginTop: 10,
+    },
+    locationDetails:{
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      flex: 1,
+    },
+    location:{
+      fontSize: 15,
+      fontWeight: '600',
+      marginTop: 5,
+      marginBottom: 40,
+    },
+    right:{
+     marginLeft: 70,
+      marginTop: 5,
+      color: '#0000FF'
+    }
   });
   
   export default reviewScreen;

20c9000904fc092cfab47364e17a09d0ef86fc59
Joppe Meijers <joppe@pixelpros.nl>
2019-10-07 16:14:24 +0200

    fix review

--- src/reviewScreen.js
+++ src/reviewScreen.js
@@ -1,10 +1,8 @@
 import React, {Component} from 'react';
-
 import {Platform, StyleSheet, Text, View, Image,ScrollView} from 'react-native';
 import SafeAreaView from 'react-native-safe-area-view';
 import { Avatar, Divider,Button, List, ListItem, Rating } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
 
 
 
@@ -33,7 +31,6 @@ class reviewScreen extends Component {
         <ScrollView>
           <SafeAreaView>
         <View style={styles.container}>
-
           <Text style={styles.locationName}>{this.name}</Text>  
           <Text style={styles.date}>{this.date}</Text>
            <Rating type='star' ratingCount={5} imageSize={10} startingValue={this.rating} readonly style={styles.rating} />
@@ -49,7 +46,6 @@ class reviewScreen extends Component {
             </View>
             <Button style={styles.changeButton}  title="Bewerk review" />
             <Button style={styles.changeButton}  title="Voeg foto toe" />
-
         </View>
         </SafeAreaView>
       </ScrollView>
@@ -60,53 +56,52 @@ class reviewScreen extends Component {
   const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        padding: 30,
         paddingTop: 20,
-    },
-    welcome:{
-      marginTop: 15,
-        color: 'black',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    userName:{
-      fontWeight: '800',
-    },
-      information:{
-        marginTop: 30,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        flex: 1,
-
-    },
-    block:{
-      marginLeft: 35,
-      marginRight: 35,
-      alignItems: 'center',
-      textAlign: 'center',
-    },
-    counting:{
-      marginTop: 10,
-      fontWeight: '800',
-      fontSize: 21,
-    },
-    title:{
-      color: '#000',  
-      textAlign: 'left',
-      fontSize: 15,
-      fontWeight: '600',
-      marginTop: 30,
-      marginLeft: 20,
-    },
-    reviews :{
-      marginLeft: 10,
     },
     rating:{
         marginTop: 5,
         marginBottom: 5,
         alignItems: 'flex-start',
       },
+    divider:{
+      backgroundColor: 'grey',
+      marginTop: 15,
+      marginBottom: 15,
+    },
+    locationName:{
+      fontWeight: '800',
+      fontSize: 21
+    },
+    date:{
+      fontStyle: 'italic',
+      marginTop: 5,
+      fontWeight: '300',
+    },
+    subjectName:{
+      fontWeight: '600',
+      fontSize: 16,
+      marginBottom: 5,
+    },
+    changeButton:{
+      marginTop: 10,
+    },
+    locationDetails:{
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      flex: 1,
+    },
+    location:{
+      fontSize: 15,
+      fontWeight: '600',
+      marginTop: 5,
+      marginBottom: 40,
+    },
+    right:{
+     marginLeft: 70,
+      marginTop: 5,
+      color: '#0000FF'
+    }
   });
   
   export default reviewScreen;
